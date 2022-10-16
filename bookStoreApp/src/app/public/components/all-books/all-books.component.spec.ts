49d0dd58fd1dfad207583aee8d9742d756c2fc98
Saranya <ragusfs@gmail.com>
2022-10-16 15:11:57 +0530

    Multiple Modules Created

---
+++ bookStoreApp/src/app/public/components/all-books/all-books.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AllBooksComponent } from './all-books.component';
+
+describe('AllBooksComponent', () => {
+  let component: AllBooksComponent;
+  let fixture: ComponentFixture<AllBooksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AllBooksComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AllBooksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
