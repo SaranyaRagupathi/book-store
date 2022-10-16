ca3c773cff2282bac90cfe73cbc4516d1d2c0a3b
Saranya <ragusfs@gmail.com>
2022-10-16 16:30:33 +0530

    Routing Enabled

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { AppComponent } from './app.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 
+const routes: Routes = [
+  {path:'about-us',component:AboutUsComponent},
+  {path:'how-it-works',component:HowItWorksComponent}
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
