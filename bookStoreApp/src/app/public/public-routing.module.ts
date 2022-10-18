5bd5b83d1f6d119bee885c1236d0ff9ed1a42586
Saranya <ragusfs@gmail.com>
2022-10-18 12:56:15 +0530

    Get Parameters value from route

--- bookStoreApp/src/app/public/public-routing.module.ts
+++ bookStoreApp/src/app/public/public-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicComponent } from './public.component';
 import { AllBooksComponent } from './components/all-books/all-books.component';
@@ -7,7 +6,9 @@ import { BookDetailsComponent } from './components/book-details/book-details.com
 
 const routes: Routes = [
   {
-    path: 'public', component: PublicComponent, children: [
+    path: 'public',
+    component: PublicComponent,
+    children: [
       { path: 'all-books', component: AllBooksComponent },
       { path: 'book-details/:id/author/:authorId', component: BookDetailsComponent }
     ]
