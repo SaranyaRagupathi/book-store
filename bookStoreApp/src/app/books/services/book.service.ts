1e44f6e9b258a7aaa44979c60a1dd6f3668233bb
Saranya <ragusfs@gmail.com>
2022-12-13 12:00:36 +0530

    Mat Card Design Updated

--- bookStoreApp/src/app/books/services/book.service.ts
+++ bookStoreApp/src/app/books/services/book.service.ts
@@ -48,6 +48,36 @@ export class BookService {
           "currency": "INR",
           "value": 80
         }
+      },
+      {
+        "id": 2,
+        "title": 'Java Fundamentals',
+        "totalPages": 340,
+        "author": 'Herbert Schildt',
+        "price": {
+          "currency": "USD",
+          "value": 23
+        }
+      },
+      {
+        "id": 3,
+        "title": 'HTML & CSS',
+        "totalPages": 680,
+        "author": 'John Duckett',
+        "price": {
+          "currency": "INR",
+          "value": 400
+        }
+      },
+      {
+        "id": 4,
+        "title": 'The Secret of JavaScript',
+        "totalPages": 230,
+        "author": 'Ram Singh',
+        "price": {
+          "currency": "INR",
+          "value": 80
+        }
       }
     ]
   }
