4340093fa649ff389971780fe5e5ad7a0d15dbf9
Saranya <ragusfs@gmail.com>
2022-12-23 11:56:04 +0530

    Difference between ProvidedIn root and any

--- bookStoreApp/src/app/books/components/all-books/all-books.component.ts
+++ bookStoreApp/src/app/books/components/all-books/all-books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CounterService } from 'src/app/shared/services/counter.service';
 import { BookModel } from '../../models/book.model';
 import { BookService } from '../../services/book.service';
 
@@ -9,12 +10,18 @@ import { BookService } from '../../services/book.service';
 })
 export class AllBooksComponent implements OnInit {
 public books:BookModel[]=[];
-  constructor(public bookService:BookService) { 
+  constructor(public bookService:BookService,public _counterService:CounterService) { 
   }
 
   ngOnInit(): void {
     this.books = this.bookService.getBooks();
     console.log(this.books);
+  }
+  public increase():void{
+    this._counterService.incCounter();
+  }
+  public decrease():void{
+    this._counterService.decCounter();
   }
 
 }
